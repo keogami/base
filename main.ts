a635725f95da23747c4c1efdfa6b7131040d30c0
keogami <41939011+keogami@users.noreply.github.com>
2022-12-01 14:31:07 +0530

    feat: implement the encoding and the decoding logic

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
-export function add(a: number, b: number): number {
-  return a + b;
+class DecodeError extends Error {
+  constructor(char: string) {
+    super(`"${char}" is not found in the encoding map.`);
+    this.name = "DecodeError";
+  }
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export class Base {
+  #map: string;
+  #len: number;
+  #idx: Map<string, number>;
+
+  constructor(map: string) {
+    this.#len = map.length;
+    this.#map = map;
+    this.#idx = new Map(
+      map.split("").map((val, idx) => [val, idx]),
+    );
+  }
+
+  encode(num: number): string {
+    return num < this.#len
+      ? this.#map[num]
+      : (this.encode(Math.floor(num / this.#len)) + this.#map[num % this.#len]);
+  }
+
+  decode(str: string): number {
+    return str.split("").reverse().map((val, idx) => {
+      const numval = this.#idx.get(val);
+      if (typeof numval === "undefined") {
+        throw new DecodeError(val);
+      }
+
+      return numval * (this.#len ** idx);
+    }).reduce((a, b) => a + b);
+  }
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,29 @@
-import { assertEquals } from "https://deno.land/std@0.166.0/testing/asserts.ts";
-import { add } from "./main.ts";
+import { Base } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test("That encode and decode are inverse operations", async _t => {
+  const base = new Base("01");
+  const check = (i: number) => {
+    return {
+      correct: base.decode(base.encode(i)) === i,
+      i,
+    };
+  };
+
+  const all: Promise<ReturnType<typeof check>>[] = [];
+  for (let i = 0; i < 2 ** 16; i++) {
+    all.push(new Promise((res) => res(check(i))));
+  }
+
+  const bad = (await Promise.all(all)).filter(({ correct }) => !correct);
+  if (bad.length === 0) return;
+
+  bad.slice(0, 8).map(({ i }) =>
+    console.log(`encode and decode are not inverse for ${i}`)
+  );
+
+  if (bad.length > 8) {
+    console.log("there were more errors than 8");
+  }
+
+  throw new Error("mau")
 });
